4b21ba86531b34f43f408829ad405d1908973029
Tomasz Mendrun <337371@uwr.edu.pl>
2022-10-26 13:18:58 +0200

    poprawka

--- Lista1/Zad10a.py
+++ Lista1/Zad10a.py
@@ -1,14 +1,14 @@
 import cmath
-z=int(input())
-s=complex(cmath.sin(z))
-c=complex(cmath.cos(z))
+z=complex(input())
+s=cmath.sin(z)
+c=cmath.cos(z)
 
-print(s)
-print(c)
+print(s.real, s.imag)
+print(c.real, c.imag)
 
 print("Dodaje sin^2(z) do cos^2(z)")
+print(s**2 + c**2)
 if (s**2 + c**2 == 1):
-    print(s**2 + c**2)
     print("sin^2(z) do cos^2(z) wynosi 1")
 else:
     print("Nope! It ain't working.")
