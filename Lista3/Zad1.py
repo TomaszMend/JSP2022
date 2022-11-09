5161dfc23ce0820194f71b517deba5608b8b24c1
Tomasz Mendrun <337371@uwr.edu.pl>
2022-11-09 11:35:47 +0100

    Nowa_lista

---
+++ Lista3/Zad1.py
@@ -0,0 +1,7 @@
+sam = ["a","e","i","o","u","y","ą","ę","ó"]
+lit = lower(input("Podaj literę:"))
+if lit in sam:
+	print("Podana litera jest samogłoską")
+else:
+	print("Podana litera jest współgłoską")
+
